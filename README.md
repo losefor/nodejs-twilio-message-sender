c4f123a15dfeeaa1826c60bc64e052170fc14919
muhammed paqer <moh999157@gmail.com>
2020-07-28 12:53:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## message sende with twilio and node 
+
+### how to run the app 
+```
+- need to sign in with twilio account - get your own phone number, accessToken and authToken from your account and change the requirements in the app.js 
+- run $npm install - so you can install dependencies 
+- run the app by running $node app.js
+```
+
+### how to use the app
+**go to the following route**
+```
+message/[your own message goes here]/[the phone number that you want to send the message to]
+```
---
+++ app.js
@@ -0,0 +1,27 @@
+//these two variables need to be edited
+const accountSid = "Here goes your account sid";
+const authToken = "Here goes your auth token";
+
+const app = require("express")();
+const client = require("twilio")(accountSid, authToken);
+
+app.get("/message/:message/:phoneNumber", (req, res) => {
+  client.messages
+    .create({
+        
+        //this from key need to be changed
+      from: "Here goes your twillio phone number",
+      body: req.params.message,
+      to: `+964${req.params.phoneNumber}`,
+    })
+    .then((message) => {
+      res.json(message);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(5050, () => {
+  console.log("started on port 5050");
+});
