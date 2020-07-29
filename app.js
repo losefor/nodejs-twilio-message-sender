cc546e3f7c2b61bf675f0246e5e0ed1f0e3c1c32
muhammed paqer <moh999157@gmail.com>
2020-07-29 22:28:40 +0300

    new changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //these two variables need to be edited
-const accountSid = "Here goes your account sid";
-const authToken = "Here goes your auth token";
+const accountSid = "AC0beb3a487c9fdd204f74b5cb26cc9976";
+const authToken = "09df2b97732331ccd09e151053791b7d";
 
 const app = require("express")();
 const client = require("twilio")(accountSid, authToken);
@@ -10,7 +10,7 @@ app.get("/message/:message/:phoneNumber", (req, res) => {
     .create({
         
         //this from key need to be changed
-      from: "Here goes your twillio phone number",
+      from: "+12055832557",
       body: req.params.message,
       to: `+964${req.params.phoneNumber}`,
     })
